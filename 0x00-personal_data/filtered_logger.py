c44a9334b62afaa0466f6759038ace1313626cbd
OmobaVII <omoba.career@gmail.com>
2023-11-02 11:46:09 +0000

    Initial Commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,10 +4,11 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ returns the log message obfuscated """
     for field in fields:
         message = re.sub(field + "=.*?" + separator,
                          field + "=" + redaction + separator,
                          message)
-        return message
+    return message
