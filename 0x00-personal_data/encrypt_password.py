0be02c7a17de0791c2a35c4a79a6e4a4aac29e94
OmobaVII <omoba.career@gmail.com>
2023-11-02 18:26:17 +0000

    Encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """
-Provides a function that prevents password from 
-being stored in plain text in a databse
+  Provides a function that prevents password from
+  being stored in plain text in a databse
 """
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """ a function used to encrypt a password """
     salt = bcrypt.gensalt()
-    hashed_pwd = bcrypt.hashpw(password.encode('utf-8'), salt)
+    hashed_pwd = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_pwd
