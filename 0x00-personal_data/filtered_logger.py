de12a3559cbecec0ad6ff516cee31094dd48bf35
OmobaVII <omoba.career@gmail.com>
2023-11-02 11:34:23 +0000

    Initial Commit

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+This module provide a function `filter_datum`
+the function returns the log message obfuscated
+"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """ returns the log message obfuscated """
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator,
+                         message)
+        return message
