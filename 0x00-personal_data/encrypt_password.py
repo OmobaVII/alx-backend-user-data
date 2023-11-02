6429132741c24831462b67956a813037578967d8
OmobaVII <omoba.career@gmail.com>
2023-11-02 18:40:01 +0000

    check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """
-  Provides a function that prevents password from
-  being stored in plain text in a databse
+  Provides hashed_password function that prevents password from
+  being stored in plain text in a database
+
+  Provides is_valid function that checks that the password
+  matches the hashed password
 """
 import bcrypt
 
@@ -12,3 +15,11 @@ def hash_password(password: str) -> bytes:
     hashed_pwd = bcrypt.hashpw(password.encode(), salt)
 
     return hashed_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ checks that the password matches the hashed password """
+    try:
+        return bcrypt.checkpw(password.encode(), hashed_password)
+    except (ValueError, TypeError):
+        return False
