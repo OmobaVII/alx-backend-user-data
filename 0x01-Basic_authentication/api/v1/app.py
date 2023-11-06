46d6820ec5bea800243f29b516af5d230b0daf1d
OmobaVII <omoba.career@gmail.com>
2023-11-06 21:28:50 +0000

    Initial Commit

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-if getenv('AUTH_TYPE'):
+if getenv('AUTH_TYPE') == 'auth':
     auth = Auth()
 else:
     auth = None
