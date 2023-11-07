817bcbc21c76cef41ee468b421670ac85719d125
OmobaVII <omoba.career@gmail.com>
2023-11-07 10:16:14 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ This module provides the class BasicAuth """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,17 @@ class BasicAuth(Auth):
 
         end_part = authorization_header.replace('Basic ', '', 1)
         return end_part
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """ returns the decoded value of a Base64 string """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            base64.b64encode(base64.b64decode(base64_authorization_header))
+        except Exception:
+            return None
+        return base64.b64decode(base64_authorization_header).decode('utf-8')
