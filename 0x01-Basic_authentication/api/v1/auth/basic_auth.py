4e83f87ac5bd6ba7f26d407311a5f3fe1df335aa
OmobaVII <omoba.career@gmail.com>
2023-11-07 09:43:29 +0000

    Basic Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" This module provides the class BasicAuth """
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ defines the class BasicAuth which inherits from Auth """
