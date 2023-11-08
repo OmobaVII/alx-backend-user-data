bcc0040fe697035398e7f59e233eafea1fc68281
OmobaVII <omoba.career@gmail.com>
2023-11-08 22:57:45 +0000

    Task 0 - 8

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -58,8 +58,7 @@ def before_request():
         return
     if not auth.require_auth(request.path, exclu_path):
         return
-    if auth.authorization_header(request) is None:
-        abort(401)
+
     if auth.authorization_header(request) is None\
             and auth.session_cookie(request) is None:
         abort(401)
