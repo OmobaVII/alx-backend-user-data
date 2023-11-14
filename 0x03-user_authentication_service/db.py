72dcffc7a8d606d7baa5693b948d1873be712a64
OmobaVII <omoba.career@gmail.com>
2023-11-14 07:55:12 +0000

    Add find user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -51,8 +51,8 @@ class DB:
         """ Finds a user in the db based on the keyword argument """
         try:
             user = self._session.query(User).filter_by(**kwargs).first()
-            if user is None:
-                raise NoResultFound
             return user
+        except NoResultFound:
+            raise
         except InvalidRequestError:
             raise
