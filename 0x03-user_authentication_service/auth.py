8a25dd6e584ee1a2061afd22477b9c20be59a072
OmobaVII <omoba.career@gmail.com>
2023-11-15 09:37:45 +0000

    Add the update password method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,4 +104,3 @@ class Auth:
             raise ValueError
         pwd = _hash_password(password)
         self._db.update_user(user.id, hashed_password=pwd, reset_token=None)
-        return None
